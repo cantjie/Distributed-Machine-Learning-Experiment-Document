94d837aca3634bc1111035423572596c40f9112d
Jesslyn Nathania <jesslynnathania99@gmail.com>
2023-06-02 08:24:46 +0700

    temp commit

--- codes/task1/pytorch/MyOptimizer.py
+++ codes/task1/pytorch/MyOptimizer.py
@@ -1,3 +1,30 @@
+from torch.optim import SGD, Adam
+
+import torch
+
+#TODO: now
+
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)   
+        self.predict = torch.nn.Linear(n_hidden, n_output)  
+
+    def forward(self, x):
+        x = F.relu(self.hidden(x))      
+        x = self.predict(x)             
+        return x
+
+criterion = torch.nn.MSELoss()
+
+
+#  Gradient Descent (GD):
+net_all = Net(n_feature=1, n_hidden=10, n_output=1) 
+optimizer_all = torch.optim.SGD(net_all.parameters(), lr=0.2)
+
+net_item = Net(n_feature=1, n_hidden=10, n_output=1) 
+optimizer_item = torch.optim.SGD(net_item.parameters(), lr=0.2)
+
 class BaseOptimizer():
     def __init__(self, params, lr=0.001):
         self.params = list(params)
